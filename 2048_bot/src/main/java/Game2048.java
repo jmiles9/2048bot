e5cf1da7dd34f588d78f9fd429396732b232e050
Miles Justice <justicemiles729@gmail.com>
2019-10-18 17:25:49 -0700

    project initialized

---
+++ 2048_bot/src/main/java/Game2048.java
@@ -0,0 +1,14 @@
+
+
+
+
+
+/**
+ * Game of 2048
+ * Should pop up a window that the user can control with key presses. (need to
+ * be able to change this to neural net input later.... )
+ * returns: final score???
+ */
+public class Game2048 {
+    
+}
